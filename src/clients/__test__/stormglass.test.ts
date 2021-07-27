54627d6616f02b21f7b1f6799b09abb0512060d3
Caio Martins <dev.caiomartins@gmail.com>
2021-07-27 18:51:39 +0100

    Creating api client to consume external stormglass api

---
+++ src/clients/__test__/stormglass.test.ts
@@ -0,0 +1,20 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursMock from '@test/mocks/stormglass_weather_3_hours.json';
+import stormGlassWeather3HoursNormalizedMock from '@test/mocks/stormglass_weather_3_hours_normalized.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursMock);
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual(stormGlassWeather3HoursNormalizedMock);
+  });
+});
